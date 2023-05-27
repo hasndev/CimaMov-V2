3c8196333d236e853a5406ad4b16ac0cb426bca0
Hassan J. Oudah <106507700+hasnja@users.noreply.github.com>
2023-05-27 12:20:15 +0300

    Update Details.vue

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -61,7 +61,7 @@
 					</div>
 				</div>
 				<!-- Movie Section -->
-				<div class="bg-gray-900 py-16">
+				<div class="py-16">
 					<div class="container mx-auto px-4">
 						<h2 class="text-3xl font-bold mb-8 text-center text-red">Related Movies</h2>
 						<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
